b41d534a7713a28046e7a53d15f65c89f351771e
Aditya Kr. Singh <adityalko2004@gmail.com>
2026-03-11 16:37:56 +0530

    prepare backend for deployment

--- db.py
+++ db.py
@@ -1,5 +1,17 @@
-import psycopg2 # type: ignore
-from config import DB_CONFIG
+# import psycopg2 # type: ignore
+# from config import DB_CONFIG
+
+# def get_connection():
+#     return psycopg2.connect(**DB_CONFIG)
+
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(**DB_CONFIG)
+    conn = psycopg2.connect(
+        os.getenv("DATABASE_URL")
+    )
+    return conn
